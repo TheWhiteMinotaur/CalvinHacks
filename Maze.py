858c26ac4b268c4fd577004563f134bdefa420c7
Silas A <agnewsilas@gmail.com>
2020-02-22 01:01:08 -0500

    Config parsing

--- Maze.py
+++ Maze.py
@@ -1,21 +1,24 @@
 # import Enemy from enemy
 import pygame
 
+def parse_config():
+    with open('config.cfg', 'r') as config_file:
+        lines = config_file.read().split('\n')
+        return [l.split('=') for l in lines]
+
 def main():
     print("main!")
 
+    try:
+        configs = parse_config()
+    except:
+        print("Error parsing config file.")
+        return
+
     # enemy = Enemy(100, 0, 0, pygame.color.red)
 
-    (width, height) = (300, 200)
+    (width, height) = (str(configs[0]), str(configs[1]))
     screen = pygame.display.set_mode((width, height))
-    # pygame.display.flip()
-    screen.fill((250, 0, 0))
-
-    # pygame.draw.circle(screen, enemy.color, (enemy.x, enemy.y), enemy.size)
-    pygame.draw.circle(screen, (0, 250, 0), [10, 10], 10, 1)
-
-
-
 
     pygame.display.flip()
 
@@ -26,6 +29,5 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
 
 main()
